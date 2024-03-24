98b9bcb55b55293f1f1e24d467b0c98533a1bd26
Francisco Souza <assis.souza@gmail.com>
2024-03-24 13:29:05 -0300

    fix: set local variable instead localstorage for test

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -22,16 +22,14 @@ interface MarkerInterface {
     }
 }
 
+const deaLocations:MarkerInterface[] = [{"lat":-15.633020275933195,"lng":-48.02632917519532,"address":"Bem aqui","DEA":"Zoll","comar":{"id":3,"descr":"COMAR III"},"gbm":{"id":17,"descr":"17º GBM"}},{"lat":-15.903622638765505,"lng":-47.77651011943817,"address":"17º GBM","DEA":"Zoll","comar":{"id":2,"descr":"COMAR II"},"gbm":{"id":12,"descr":"12º GBM"}},{"lat":-15.796168187860095,"lng":-47.89188058289393,"address":"Pátio Brasil","DEA":"Phillips","comar":{"id":2,"descr":"COMAR I"},"gbm":{"id":1,"descr":"1º GBM"}}]
+
 const Index: FC = () => {
-
-
-    let deaLocations = localStorage.getItem('deas') as string;
-    const deaLocation = deaLocations ? JSON.parse(deaLocations) : [];
 
     const [filteredDeaStore, setFilteredDeaStore] = useState(Array<MarkerInterface>);
 
     const handleComarFilter = (comar: number) => {
-        const filtered = deaLocation.filter((dea: MarkerInterface) => dea.comar.id == comar);
+        const filtered = deaLocations.filter((dea: MarkerInterface) => dea.comar.id == comar);
         setFilteredDeaStore(filtered);
     }
 
@@ -42,7 +40,7 @@ const Index: FC = () => {
                 <Header />
                 <FilterDrawer handleComarFilter={handleComarFilter} />
             </FilterDrawerContextProvider>
-            <Mapa deaStore={deaLocation} filteredDeaStore={filteredDeaStore} />
+            <Mapa deaStore={deaLocations} filteredDeaStore={filteredDeaStore} />
         </div>
     )
 
